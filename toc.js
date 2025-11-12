35e1b00e4216b3dba73940e9841f20675df38ef7
zimkov <zimkov@users.noreply.github.com>
2025-11-12 12:11:38 +0000

    deploy: 795558ed2c3386a9df7c05bfdda6202a4a9b66ba

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="technical_specification.html"><strong aria-hidden="true">1.</strong> ТЗ</a></li><li class="chapter-item expanded "><a href="md.html"><strong aria-hidden="true">2.</strong> Конфигурация проекта</a></li><li class="chapter-item expanded "><a href="drawio.html"><strong aria-hidden="true">3.</strong> Пример использования Drawio</a></li><li class="chapter-item expanded "><a href="swagger.html"><strong aria-hidden="true">4.</strong> Пример использования Swagger</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="technical_specification.html"><strong aria-hidden="true">1.</strong> ТЗ</a></li><li class="chapter-item expanded "><a href="c4.html"><strong aria-hidden="true">2.</strong> C4</a></li><li class="chapter-item expanded "><a href="md.html"><strong aria-hidden="true">3.</strong> Конфигурация проекта</a></li><li class="chapter-item expanded "><a href="drawio.html"><strong aria-hidden="true">4.</strong> Пример использования Drawio</a></li><li class="chapter-item expanded "><a href="swagger.html"><strong aria-hidden="true">5.</strong> Пример использования Swagger</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
